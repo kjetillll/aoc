ea9ac22d7d204fb85bc19eae37d7b153a3621484
KjetilS <kjetilskotheim@gmail.com>
2023-12-26 01:52:49 +0100

    .

--- advent_of_code_2023/2023_day_12_part_2.pl
+++ advent_of_code_2023/2023_day_12_part_2.pl
@@ -1,13 +1,26 @@
-# perl 2023_day_12_part_2.pl -p 1 2023_day_12_ex.txt    # -p 1    => 21
-# perl 2023_day_12_part_2.pl -p 2 2023_day_12_ex.txt    # -p 2    => 525152
-# perl 2023_day_12_part_2.pl -p 1 2023_day_12_input.txt # part 1  => 7460             0.75 sek uten memoize
-# perl 2023_day_12_part_2.pl -p 1 2023_day_12_input.txt # part 1  => 7460             0.12 sek med memoize
-# perl 2023_day_12_part_2.pl -p 2 2023_day_12_input.txt # part 2  =>                  uendelig tid uten memoize
-# perl 2023_day_12_part_2.pl -p 2 2023_day_12_input.txt # part 2  => 6720660274964    3.1 sek med memoize
+# https://adventofcode.com/2023/day/12 - del 2
+# kjør:
+#
+# perl 2023_day_12_part_2.pl 2023_day_12_ex.txt      # svar: 525152                0.01 sek
+# perl 2023_day_12_part_2.pl 2023_day_12_input.txt   # svar: 6720660274964         1.54 sek
 
 use strict; use warnings; no warnings 'recursion';
+
+my $pratsom = 0;
 my $part = "@ARGV" =~ /^-p ([12])/ && splice(@ARGV,0,2) ? $1 : 2;
 my($sum, $arr, %cache);
+
+while(<>){
+    my($p,$d) = /\S+/g; #pattern, dammaged-groups
+    $p = join '?', ($p) x ( $part == 2 ? 5 : 1 );
+    $d = join ',', ($d) x ( $part == 2 ? 5 : 1 );
+    my @d = $d =~ /\d+/g;
+    my $arr = arr( $p, @d );
+    $sum += $arr;
+    printf "L%-4d sum: %13d   arr: %10d   cache: %-5d   p: $p   d: @d\n", $., $sum, $arr, 0+keys(%cache) if $pratsom;
+}    
+
+print "svar: $sum\n";
 
 sub arr {                                               #retur: antall arrangementer
     $cache{"@_"} //= do {                               #memoize/cache og gjenbruk resultatet
@@ -20,14 +33,3 @@ sub arr {                                               #retur: antall arrangeme
        :                 !($p||@d)
    }
 }
-
-while(<>){
-    my($p,$d) = /\S+/g; #pattern, dammaged-groups
-    $p = join'?', ($p) x ($part==2 ? 5 : 1);
-    $d = join',', ($d) x ($part==2 ? 5 : 1);
-    my @d = $d=~/\d+/g;
-    my $arr = arr($p,@d);
-    $sum += $arr;
-    printf "L%-4d sum: %13d   arr: %10d   cache: %-5d   p: $p   d: @d\n", $., $sum, $arr, 0+keys(%cache);
-}    
-print "svar: $sum\n";
