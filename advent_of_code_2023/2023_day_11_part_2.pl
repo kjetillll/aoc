9713f8c7ab04705893d78ac16cdac4da82bd2a5a
KjetilS <kjetilskotheim@gmail.com>
2023-12-26 01:37:22 +0100

    .

--- advent_of_code_2023/2023_day_11_part_2.pl
+++ advent_of_code_2023/2023_day_11_part_2.pl
@@ -1,25 +1,24 @@
 # https://adventofcode.com/2023/day/11 - del 2
 # kjør:
 #
-# perl 2023_day_11_part_2.pl 2023_day_11_input.txt  # svar: 717878258016               0.75 sek
+# perl 2023_day_11_part_2.pl 2023_day_11_input.txt   # svar: 717878258016               0.75 sek
 
-# perl 2023_day_11_part_2_ryddet.pl 2023_day_11_input.txt    # 1.3 sek => 717878258016
+use List::Util 'sum'; use Algorithm::Combinatorics 'combinations'; use strict; use warnings; use v5.10;
 
-use v5.10; use List::Util 'sum'; use Algorithm::Combinatorics 'combinations';
+my $kart = join'', <>;                               # $kart = slurp universet
+my $span_empty = 1_000_000;                          # bruk 2 for day 11 part 1
+my @rad_span = linje_span( $kart );                  # ikke-tomme rader blir 1, tomme blir 1e6
+my @kol_span = linje_span( transpose($kart) );       # ikke-tomme kolonner blir 1, tomme blir 1e6
 
-$_ = join'',<>;                            # $_ = slurp universet
-my $span_empty = 1_000_000;                # bruk 2 for day 11 part 1
-my @rad_span = linje_span($_);              # ikke-tomme rader blir 1, tomme blir 1e6
-my @kol_span = linje_span( transpose($_) ); # ikke-tomme kolonner blir 1, tomme blir 1e6
-
-my $wi = /.*/ ? 1+length$& : die;          # width
-my @galpos = posisjoner($_,'#');           # finn posisjonene til galaksene
+my $wi = $kart=~/.*/ ? 1+length$& : die;             # width
+my @galpos = posisjoner($kart,'#');                  # finn posisjonene til galaksene
 my @par = combinations(\@galpos, 2);
-say "svar: ", sum map dist(@$_), @par;
+
+say "svar: ", sum grep defined, map dist(@$_), @par; # hm grep defined
 
 sub linje_span { map /#/ ? 1 : $span_empty, split /\n/, shift }
 sub pos2xy     { my($pos,$wi) = @_; ( int($pos / $wi), $pos % $wi ) }
-sub posisjoner { my @p; $_[0] =~ s/$_[1]/push@p,pos;$&/ge; @p }
+sub posisjoner { my($s,$t) = @_; my @p; $s =~ s/$t/ push @p, pos$s; $& /ge; @p }
 sub dist {
     my($x1,$y1, $x2,$y2) = map pos2xy($_,$wi), @_;
     sum ( map $rad_span[$_], $x1<$x2 ? ($x1+1..$x2) : ($x2+1..$x1) ),
