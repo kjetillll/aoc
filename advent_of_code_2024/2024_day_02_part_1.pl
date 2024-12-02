997783124db9b53f9bdeff2b8db1fd9da140520f
KjetilS <kjetilskotheim@gmail.com>
2024-12-02 18:34:08 +0100

    hm

--- advent_of_code_2024/2024_day_02_part_1.pl
+++ advent_of_code_2024/2024_day_02_part_1.pl
@@ -1,7 +1,7 @@
 use v5.10;
 sub unike { my %sett; grep !$sett{$_}++, @_ }
 sub skli { my($s,$w)=@_; map [@$s[$_..$_+$w-1]], 0..@$s-$w }
-my %status=qw(-1 N -2 N -3 N 1 O 2 O 3 O); #N=ned O=opp 0=gal diff
-say 0+grep join("",unike(map{ $status{ $$_[1] - $$_[0] } || 0 } skli([split],2))) =~ /^[ON]$/, <>;
+my %status=qw(-1 D -2 D -3 D 1 U 2 U 3 U); #D=ned U=opp 0=gal diff
+say 0+grep join("",unike(map{ $status{ $$_[1] - $$_[0] } || 0 } skli([split],2))) =~ /^[DU]$/, <>;
 
 #359
--- advent_of_code_2024/2024_day_02_part_2.pl
+++ advent_of_code_2024/2024_day_02_part_2.pl
@@ -1,8 +1,8 @@
 use v5.10;
 sub unike { my %sett; grep !$sett{$_}++, @_ }
 sub skli { my($s,$w)=@_; map [@$s[$_..$_+$w-1]], 0..@$s-$w }
-sub en_fjernet{map{my@a=@_;splice(@a,$_,1);[@a]}0..$#_}
-my %status=qw(-1 N -2 N -3 N 1 O 2 O 3 O); #N=ned O=opp 0=gal diff
-say 0+grep{grep{join("",unike(map{ $status{ $$_[1] - $$_[0] } || 0 } skli($_,2))) =~ /^[ON]$/} en_fjernet(split)}<>;
+sub en_fjernet { map{ my @a=@_; splice @a,$_,1; \@a} 0..$#_ }
+my %status=qw(-1 D -2 D -3 D 1 U 2 U 3 U); #D=ned U=opp 0=gal diff
+say 0+grep{grep{join("",unike(map{ $status{ $$_[1] - $$_[0] } || 0 } skli($_,2))) =~ /^[DU]$/} en_fjernet(split)}<>;
 
 #418
