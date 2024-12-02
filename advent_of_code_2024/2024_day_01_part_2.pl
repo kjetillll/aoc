44ef7f013b8d56b49bc46b99d7d9c9ab8353909f
KjetilS <kjetilskotheim@gmail.com>
2024-12-02 02:52:59 +0100

    hm

--- advent_of_code_2024/2024_day_01_part_2.pl
+++ advent_of_code_2024/2024_day_01_part_2.pl
@@ -1,7 +1,3 @@
-my @inp=<>;
-my %times; $times{$_}++ for map /\d+$/g, @inp;
-printf "Result: %d\n", eval join '+', map $_ * $times{$_}, map /^\d+/g, @inp;
-__END__
-Run:
-perl 2024_day_01_part_2.pl 2024_day_01_input.txt 
-Result: 23927637
+print eval join'+',map s/\d+$/$f{$&}++;"*\$f{0+'$_'}"/er,<>
+
+#23927637
