8616d690518a33139b51202f2b225599c6783cad
Kjetil Skotheim <kjetilskotheim@gmail.com>
2024-12-06 07:39:06 +0100

    hm

--- advent_of_code_2024/2024_day_06_part_1.pl
+++ advent_of_code_2024/2024_day_06_part_1.pl
@@ -1,13 +1,13 @@
 use v5.10;
-use Acme::Tools 'srlz';
-($x,$y)=(0,0);
+
+($x,$y) = (0,0);
 while(<>){
     chomp;
     $w=length; $w!~/^(130|10)$/ and die;
     $grid{$x++,$y}=$_ for /./g;
     $x=0;$y++;    
 }
-#print srlz(\%grid,'grid','',1);
+
 $grid{$_}eq'^' and ($x,$y)=/\d+/g for keys%grid;
 my $d='U';
 say "x: $x   y: $y   d: $d";
@@ -17,16 +17,14 @@ say "x: $x   y: $y   d: $d";
     D=>['L',0,1],
     L=>['U',-1,0],
     );
-while( #0 and
-       exists $grid{$x,$y} )
-{
+while( exists $grid{$x,$y} ) {
     my($next,$dx,$dy)=@{$d{$d}};
     $grid{$x,$y}=~s/[\.\^]/X/;
     $grid{$x+$dx,$y+$dy}=~/#/ and $d=$d{$d}[0] and ($dx,$dy)=@{$d{$d}}[1,2];
     $x+=$dx; $y+=$dy;
 }
 
-for$y(0..$w-1){for$x(0..$w-1){print$grid{$x,$y}}print"\n"}
+#for$y(0..$w-1){for$x(0..$w-1){print$grid{$x,$y}}print"\n"}
 
 say "Answer: ", 0+grep/X/,values%grid;
 #Answer: 4903
--- advent_of_code_2024/2024_day_06_part_2.pl
+++ advent_of_code_2024/2024_day_06_part_2.pl
@@ -1,43 +1,30 @@
 use v5.10;
-use Acme::Tools 'srlz';
-my($x,$y,%grid)=(0,0);
+my($x,$y,%grid) = (0,0);
 while(<>){
     chomp;
     $w=length; $w!~/^(130|10)$/ and die;
     $grid{$x++,$y}=$_ for /./g;
     $x=0;$y++;    
 }
-#print srlz(\%grid,'grid','',1);
-$grid{$_}eq'^' and ($startx,$starty)=/\d+/g for keys%grid;
-my %org_grid=%grid;
-my%d=(
-    U=>['R',0,-1],
-    R=>['D',1,0],
-    D=>['L',0,1],
-    L=>['U',-1,0],
-    );
-
-my $loops=0;
-for my $ox(0..$w-1){ say"ox: $ox   loops: $loops";
-for my $oy(0..$w-1){
-    %grid = %org_grid;
-    my($d,$x,$y)=('U',$startx,$starty);
-    $grid{$ox,$oy}=~s/./O/ or die;
-    #say "x: $x   y: $y   d: $d";
-    my $is_loop=0;
-    my %been;
+($startx,$starty) = /\d+/g for grep $grid{$_} eq '^', keys%grid;
+my %d = ( U => [ R , 0,-1 ],
+          R => [ D , 1, 0 ],
+          D => [ L , 0, 1 ],
+          L => [ U ,-1, 0 ] );
+my $loops = 0;
+for my $ox ( 0 .. $w-1){ say"ox: $ox   loops so far: $loops";
+for my $oy ( 0 .. $w-1){
+    my($d,$x,$y,%been) = ('U',$startx,$starty);
+    $grid{$ox,$oy} =~ s/\./O/ or next;
     while( exists $grid{$x,$y} ) {
-	last if $been{$x,$y,$d}++ and $is_loop=1;
-	my($next,$dx,$dy)=@{$d{$d}};
-	#$grid{$x,$y}=~s/[\.\^]/X/;
-	$d=$next and ($next,$dx,$dy)=@{$d{$d}} while $grid{$x+$dx,$y+$dy}=~/[#O]/;
-	$x+=$dx; $y+=$dy;
+        ++$loops and last if $been{$x,$y,$d}++;
+        my($next,$dx,$dy) = @{$d{$d}};
+        $d = $next and ($next,$dx,$dy) = @{$d{$d}} while $grid{$x+$dx,$y+$dy} =~ /[#O]/;
+        $x += $dx; $y += $dy;
     }
-    $loops += $is_loop;
-    #if($loops){	for$x(0..$w-1){for$y(0..$w-1){print$grid{$x,$y}}print"\n"} exit }
+    $grid{$ox,$oy} = '.';
 }}
-
 say "Answer: $loops";
-#Answer: 1812 wrong    #3m45s
-#Answer: 1811 wrong
-#Answer: 
+
+#time perl 2024_day_06_part_2.pl 2024_day_06_input.txt   # 1m 35sec
+#Answer: 1911
