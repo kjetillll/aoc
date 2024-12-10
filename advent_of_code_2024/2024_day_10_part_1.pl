f771442283a51ab591eedc1cf9b61a9c9d49fb21
KjetilS <kjetilskotheim@gmail.com>
2024-12-10 07:10:26 +0100

    hm

--- advent_of_code_2024/2024_day_10_part_1.pl
+++ advent_of_code_2024/2024_day_10_part_1.pl
@@ -6,14 +6,17 @@ say "Answer: " . eval join '+', map {
     my @try = ( [$startx, $starty, 0] );
     my %path9;
     while(@try){
-	my $t = shift @try;
-	my($x, $y, $h) = @$t;
-	$path9{$x, $y}++ if $h == 9;
-	push @try, map [ @$_, $h+1 ],
-                   grep $grid{join $;, @$_} == $h+1,
-	           [$x-1,$y], [$x+1,$y], [$x,$y-1], [$x,$y+1];
+        my $t = shift @try;
+        my($x, $y, $h) = @$t;
+        $path9{$x, $y}++, next if $h == 9;
+        push @try,
+            grep $grid{$$_[0], $$_[1]} == $h+1,
+            [$x-1, $y,   $h+1],
+            [$x+1, $y,   $h+1],
+            [$x,   $y-1, $h+1],
+            [$x,   $y+1, $h+1]
     }
-    0 + keys%path9
+    0 + keys %path9
 }
 grep $grid{$_} == 0,
 keys %grid;
--- advent_of_code_2024/2024_day_10_part_2.pl
+++ advent_of_code_2024/2024_day_10_part_2.pl
@@ -4,18 +4,21 @@ my($x,$y,%grid)=(0,0); while(<>){ $grid{$x++,$y} = $_ for /./g; $y++; $x=0 } #re
 say "Answer: " . eval join '+', map {
     my($startx, $starty) = /\d+/g;
     my @try = ( [$startx, $starty, 0] );
-    my $path9;
+    my $path9 = 0;
     while(@try){
-	my $t = shift @try;
-	my($x, $y, $h) = @$t;
-	$path9++ if $h == 9;
-	push @try, map [ @$_, $h+1 ],
-                   grep $grid{join $;, @$_} == $h+1,
-	           [$x-1,$y], [$x+1,$y], [$x,$y-1], [$x,$y+1];
+        my $t = shift @try;
+        my($x, $y, $h) = @$t;
+        $path9++, next if $h == 9;
+        push @try,
+            grep $grid{$$_[0], $$_[1]} == $h+1,
+            [$x-1, $y,   $h+1],
+            [$x+1, $y,   $h+1],
+            [$x,   $y-1, $h+1],
+            [$x,   $y+1, $h+1]
     }
     $path9
 }
 grep $grid{$_} == 0,
 keys %grid;
 
-#Answer: 969
+#Answer: 472
