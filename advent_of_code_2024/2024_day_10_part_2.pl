8871f31b91150b8579649f869cce0141ee1c9733
KjetilS <kjetilskotheim@gmail.com>
2024-12-10 06:56:13 +0100

    hm

---
+++ advent_of_code_2024/2024_day_10_part_2.pl
@@ -0,0 +1,21 @@
+use v5.10;
+my($x,$y,%grid)=(0,0); while(<>){ $grid{$x++,$y} = $_ for /./g; $y++; $x=0 } #read input
+
+say "Answer: " . eval join '+', map {
+    my($startx, $starty) = /\d+/g;
+    my @try = ( [$startx, $starty, 0] );
+    my $path9;
+    while(@try){
+	my $t = shift @try;
+	my($x, $y, $h) = @$t;
+	$path9++ if $h == 9;
+	push @try, map [ @$_, $h+1 ],
+                   grep $grid{join $;, @$_} == $h+1,
+	           [$x-1,$y], [$x+1,$y], [$x,$y-1], [$x,$y+1];
+    }
+    $path9
+}
+grep $grid{$_} == 0,
+keys %grid;
+
+#Answer: 969
