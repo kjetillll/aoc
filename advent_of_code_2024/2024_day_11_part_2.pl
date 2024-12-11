11e5d2a95d7e2ef648fb802408f531881eb1c3b0
KjetilS <kjetilskotheim@gmail.com>
2024-12-11 07:30:09 +0100

    hm

--- advent_of_code_2024/2024_day_11_part_2.pl
+++ advent_of_code_2024/2024_day_11_part_2.pl
@@ -4,17 +4,17 @@ say "Answer: ".sum map stones($_,1), @input;
 say "memoization keys: ".keys%memoization;
 
 sub stones {
-    my($s,$step)=@_;
-    my $len = length($s);
-    $memoization{$s,$step} //=   #much needed memoization for steps > ~30
-    $step == $steps ? $s == 0 ? 1 : $len % 2 == 0 ? 2 : 1
-  : $s == 0         ? stones(1,                   $step+1)
-  : $len % 2 == 0   ? stones(substr($s,0,$len/2), $step+1)
-                     +stones(0+substr($s,$len/2), $step+1)
-  :                   stones($s*2024,             $step+1)
+    my($stone, $step) = @_;
+    my $len = length($stone);
+    $memoization{$stone,$step} //=   #much needed memoization for steps > ~30
+    $step == $steps + 1 ? 1
+  : $stone == 0         ? stones(1,                       $step + 1)
+  : $len % 2 == 0       ? stones(substr($stone,0,$len/2), $step + 1)
+                         +stones(0+substr($stone,$len/2), $step + 1)
+  :                       stones($stone * 2024,           $step + 1)
 }
 
 # https://adventofcode.com/2024/day/11
-# time perl 2024_day_11_part_2.pl 2024_day_11_input.txt    # 0.14 sec
+# time perl 2024_day_11_part_2.pl 2024_day_11_input.txt    # 0.15 sec
 # Answer: 236804088748754
-# memoization keys: 128799
+# memoization keys: 132570
