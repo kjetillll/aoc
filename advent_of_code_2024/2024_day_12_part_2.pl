01007f9ec52072f8eee60bc1ecbc32f64a96f33f
KjetilS <kjetilskotheim@gmail.com>
2024-12-13 08:18:53 +0100

    hm

--- advent_of_code_2024/2024_day_12_part_2.pl
+++ advent_of_code_2024/2024_day_12_part_2.pl
@@ -1,40 +1,36 @@
 use v5.10; use List::Util 'sum';
+#sub dir{my@d=@_;sub{my($x,$y)=(pop()//$_)=~/\d+/g;join$;,$x+$d[0],$y+$d[1]}}
+sub dir{my($X,$Y)=@_;sub{my($x,$y)=(pop()//$_)=~/\d+/g;join$;,$x+$X,$y+$Y}}
+sub L{dir(-1,0)->(@_)}sub R{dir(1,0)->(@_)}sub U{dir(0,-1)->(@_)}sub D{dir(0,1)->(@_)}
+
 my($x,$y,%grid,%region)=(0,0); while(<>){ $grid{$x++,$y} = $gl{$x,$y,$_} = $_ for /./g; $y++; $x=0 } #read input
 my $rid=0;
 for(sort keys %grid){                            #determine region id for each garden plot
     my($x,$y,$l) = ( /\d+/g, $grid{$_} );        #x y letter
-    $has_side{$x,$y,'L'} = $l ne $grid{$x-1,$y}; #left
-    $has_side{$x,$y,'R'} = $l ne $grid{$x+1,$y}; #right
-    $has_side{$x,$y,'U'} = $l ne $grid{$x,$y-1}; #up
-    $has_side{$x,$y,'D'} = $l ne $grid{$x,$y+1}; #down
+    $has_perim{$x,$y,$$_[0]} = $l ne $grid{$$_[1]} for map[$_=>&$_("$x$;$y")], qw(L R U D);
     next if $region{$x,$y};
-    $region{$x,$y} = $l . ++$rid;                #new region id
     my @go = ("$x$;$y");
-    while( @go ){                                #spread region id to all connected same letter
-        my($gox,$goy) = split $;, shift @go;
-        next if $region{$gox,$goy} and "$gox,$goy" ne "$x,$y";
-        $region{$gox,$goy} = $region{$x,$y};
-        push @go, grep $grid{$_} eq $l, map join($;,@$_),
-            [$gox-1,$goy], [$gox+1,$goy], [$gox,$goy-1], [$gox,$goy+1];
+    while( my $go = pop @go ){                                #spread region id to all connected same letter
+        $region{$go} //= do{
+	    push @go, grep $grid{$_} eq $l, map &$_($go), qw(L R U D);
+	    $region{$_} // $l . ++$rid                        #old or if not exists new region id
+	}
     }
 }
 
-#dont count sides more than one time, that is delete sides that has same side left or above
-delete @has_side{ grep {
+my %has_side = %has_perim;
+delete @has_side{ grep { #stops double-count for perim
         my($x,$y,$dir) = /\w+/g;
-        $grid{$x,$y} eq $grid{$x-1,$y} and $has_side{$x-1,$y,$dir} or
-        $grid{$x,$y} eq $grid{$x,$y-1} and $has_side{$x,$y-1,$dir}
+        $grid{$x,$y} eq $grid{$x-1,$y} and $has_perim{$x-1,$y,$dir} or
+	$grid{$x,$y} eq $grid{$x,$y-1} and $has_perim{$x,$y-1,$dir}
     } keys %has_side };
 
 my(%area, %perimeter, %sides);  #add up
 for(keys %grid){
     my($x, $y, $region) = ( /\d+/g, $region{$_} );
     $area{$region}++;
-    $perimeter{$region} += ($region ne $region{$x-1,$y})
-                        +  ($region ne $region{$x+1,$y})
-                        +  ($region ne $region{$x,$y-1})
-                        +  ($region ne $region{$x,$y+1});
-    $sides{$region} += sum map $has_side{$x,$y,$_}, qw(L R U D);
+    $perimeter{$region} += sum map $has_perim{$x,$y,$_}, qw(L R U D);
+    $sides{$region}     += sum map $has_side{$x,$y,$_},  qw(L R U D);
 }
 say "Answer part 1: " . sum map $area{$_} * $perimeter{$_}, sort keys %area;
 say "Answer part 2: " . sum map $area{$_} * $sides{$_},     sort keys %area;
