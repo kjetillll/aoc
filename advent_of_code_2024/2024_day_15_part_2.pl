07c9e07d4bdb80dbabb0e8f80da9acd5c022458a
KjetilS <kjetilskotheim@gmail.com>
2024-12-15 19:27:50 +0100

    simplified

--- advent_of_code_2024/2024_day_15_part_2.pl
+++ advent_of_code_2024/2024_day_15_part_2.pl
@@ -4,41 +4,34 @@ use strict; use warnings; use v5.10; use List::Util qw(all sum);
 
 my($map,$moves) = split/\n\n/, join '', <>;
 my @move = $moves =~ /./g;
-$map =~ s/./{ '#'=>'##', 'O'=>'[]', '.'=>'..', '@'=>'@.' }->{$&}/ge; #part 2 map change
-my($x,$y,$w,$n,$wx,$wy,%grid)=(0,0); for($map=~/.+/g){$grid{$x++,$y}=$_ for/./g;$w//=$x;$y++;$x=0}
+$map =~ s/./{ '#'=>'##', 'O'=>'[]', '.'=>'..', '@'=>'@.' }->{$&}/ge; #part 2 map update
+my($x,$y,$w,$n,%grid)=(0,0); for($map=~/.+/g){$grid{$x++,$y}=$_ for/./g;$w//=$x;$y++;$x=0}
 ($x,$y) = $map=~/@/ ? (length($`)%($w+1),int(length($`)/($w+1))) : die;
 say "x: $x   y: $y   w: $w   moves: ".@move."   init map:"; say $map;
 
 for my $move (@move){
-    my($dx,$dy) = @{{'>',[1,0],'<',[-1,0],'v',[0,1],'^',[0,-1]}->{$move}};
+    my($dx,$dy) = @{ { '>'=>[1,0], '<'=>[-1,0], 'v'=>[0,1], '^'=>[0,-1] }->{$move} };
     say "--------------- move ".++$n." of ".@move."   x: $x   y: $y   move: $move";
-    my $pm = $move=~/<|>/ ? sub{ my $p = $move eq '<' ?']' : '[';
-				 [$grid{$wx,    $wy  } eq $p  => $wx+$dx, $wy  ],
-				 [$grid{$wx+$dx,$wy  } eq $p  => $wx+$dx, $wy  ] }
-                          : sub{ [$grid{$wx,  $wy+$dy} eq '[' => $wx,   $wy+$dy],
-				 [$grid{$wx,  $wy+$dy} eq '[' => $wx+1, $wy+$dy],
-				 [$grid{$wx,  $wy+$dy} eq ']' => $wx,   $wy+$dy],
-				 [$grid{$wx,  $wy+$dy} eq ']' => $wx-1, $wy+$dy] };
-    my @gridmoves;
-    my @work = ( [$x,$y] );
-    while(@work){
-        push @gridmoves, [ ($wx,$wy) = @{shift@work} ];
-        push @work, grep shift@$_, &$pm; #pm=potential moves
+    my(@check, @gridmoves, %visited) = ([$x,$y],,);
+    while( @check ){ #bfs
+        my($cx, $cy) = @{shift@check};
+        next if $visited{$cx,$cy}++ or index('@O[]',$grid{$cx,$cy}) == -1;
+        push @gridmoves, [$cx, $cy];
+        push @check, [$cx+1, $cy] if $grid{$cx,$cy} eq '[';
+        push @check, [$cx-1, $cy] if $grid{$cx,$cy} eq ']';
+        push @check, [$cx+$dx, $cy+$dy];
     }
     if( all{ my($x,$y)=@$_; $grid{$x+$dx,$y+$dy} ne '#' } @gridmoves ){
-        push @$_, $grid{join$;,@$_} for @gridmoves;
-        for(reverse @gridmoves){
-            my($mx,$my,$mc) = @$_;
-            my $pos     = $mx       + $my       * ($w+1);
-            my $pos_new = ($mx+$dx) + ($my+$dy) * ($w+1);
-            substr($map,$pos_new,1) = $grid{$mx+$dx, $my+$dy} = $mc;
-            substr($map,$pos,    1) = $grid{$mx,     $my    } = '.';
+        for( reverse @gridmoves ){
+            my($cx,$cy) = @$_;
+            $grid{$cx+$dx, $cy+$dy} = substr($map,$cx+$dx + ($cy+$dy) * ($w+1), 1) = $grid{$cx,$cy};
+            $grid{$cx,     $cy    } = substr($map,$cx     + $cy       * ($w+1), 1) = '.';
         }
         $x += $dx, $y += $dy if @gridmoves
     }
     say $map;
 }
-say "Answer: " . sum map{my($x,$y)=/\d+/g;$x+$y*100}grep$grid{$_}eq'[',keys%grid;
+say "Answer: " . sum map{my($x,$y)=/\d+/g;$x+$y*100}grep$grid{$_}=~/O|\[/,keys%grid;
 
 __END__
 
