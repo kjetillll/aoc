f052d41a161983523a498f673eb975ff0be7b8a3
KjetilS <kjetilskotheim@gmail.com>
2024-12-17 17:11:06 +0100

    speedup

--- advent_of_code_2024/2024_day_17_part_2.pl
+++ advent_of_code_2024/2024_day_17_part_2.pl
@@ -1,60 +1,41 @@
 use v5.10; use Carp;
 my $SAMPLES = $ENV{SAMPLES} // 1000; #hm, more is safer? for my input: min ~50, 2000 gives wrong answer
-my %op = qw( 0 adv   1 bxl   2 bst   3 jnz   4 bxc   5 out   6 bdv   7 cdv );
-
-sub run_program {
-    my($regs,@program) = @_;
-    my %r = %$regs;
-    my $ip = 0;
-    my @out;
-    my $combo = sub {
-	my $opnd = shift;
-	0 <= $opnd && $opnd <= 3 ? $opnd
-        :$opnd == 4 ? $r{A}
-	:$opnd == 5 ? $r{B}
-	:$opnd == 6 ? $r{C}
-	:$opnd == 7 ? die "Reserved, invalid opnd 7"
-        : die "Invalid combo, opnd: $opnd";
-    };
-    my $clock=0;
-    while($ip <= $#program){ $clock++;
-	my($opcode,$opnd) = @program[$ip,$ip+1];
-	my $next = $ip+2;
+sub compile {
+    my @program=@_;
+    my $perl='';
+    my $add=sub{$perl.=sprintf("    ".shift,@_).";\n"};
+    my %op = qw( 0 adv   1 bxl   2 bst   3 jnz   4 bxc   5 out   6 bdv   7 cdv );
+    for(grep $_%2==0, 0..$#program){
+	my($opcode,$opnd)=@program[$_,$_+1];
 	my $op = $op{$opcode};
-	if   ( $op eq 'adv' ){ $r{A} = int( $r{A} / 2 ** &$combo($opnd) ) }
-	elsif( $op eq 'bxl' ){ $r{B} = $r{B} ^ $opnd }
-	elsif( $op eq 'bst' ){ $r{B} = &$combo($opnd) % 8 }
-	elsif( $op eq 'jnz' ){ $next = $opnd if $r{A} != 0 }
-	elsif( $op eq 'bxc' ){ $r{B} = $r{B} ^ $r{C} }
-	elsif( $op eq 'out' ){ push @out, &$combo($opnd) % 8 }
-	elsif( $op eq 'bdv' ){ $r{B} = int( $r{A} / 2 ** &$combo($opnd) ) }
-	elsif( $op eq 'cdv' ){ $r{C} = int( $r{A} / 2 ** &$combo($opnd) ) }
-	else {die "invalid op: $op   opcode: $opcode"}
-	$ip = $next;
+	say"opcode: $opcode   opnd: $opnd";
+	my $combo = 0<=$opnd && $opnd <= 3 ? $opnd : $opnd==4 ? '$rA' : $opnd==5 ? '$rB' : $opnd==6 ? '$rC' : -1; #die"opnd: $opnd";
+	if   ( $op eq 'adv' ){ &$add('$rA = int( $rA / 2 ** %s )', $combo) }
+	elsif( $op eq 'bxl' ){ &$add('$rB = $rB ^ %s',$opnd) }
+	elsif( $op eq 'bst' ){ &$add('$rB = %s % 8',$combo) }
+	elsif( $op eq 'jnz' ){ $perl = "do{\n$perl } until \$rA == 0" }
+	elsif( $op eq 'bxc' ){ &$add('$rB = $rB ^ $rC') }
+	elsif( $op eq 'out' ){ &$add('push @out, %s % 8',$combo) }
+	elsif( $op eq 'bdv' ){ &$add('$rB = int( $rA / 2 ** %s)',$combo) }
+	elsif( $op eq 'cdv' ){ &$add('$rC = int( $rA / 2 ** %s)',$combo) }
+	else {die "op: $op"}
     }
-    push @clocks,$clock;
-    (\@out,\%r)
+    $perl = sprintf('sub run_program { my($rA,$rB,$rC) = @_; my @out; %s; return @out }',$perl);
+    die if $perl=~s/until/until/g > 1;
+    say "compiles: $perl";
+    eval $perl; $@ and die$@;
 }
 
-sub test {
-    my($out,$r);
-    my $tr = sub{(shift() ? 'ok' : 'NOT OK').' test'}; #tr test result
-    ($out,$r) = run_program( {C=>9},   2,6);         say &$tr($$r{B}==1);
-    ($out,$r) = run_program( {A=>10},  5,0,5,1,5,4); say &$tr("@$out" eq '0 1 2');
-    ($out,$r) = run_program( {A=>2024},0,1,5,4,3,0); say &$tr("@$out" eq '4 2 5 6 7 7 7 7 3 1 0' && $r{A}==0);
-    ($out,$r) = run_program( {B=>29},  1,7);         say &$tr($$r{B}==26);
-}
 my(%r,@program);
 while(<>){
     if   (/Register (\w): (\d+)/){ $r{$1} = $2 }
     elsif(/Program: (\S+)/      ){ @program = split/,/,$1 }
 }
-printf"program: %d %d $op{$program[$_]}\n", @program[$_, $_+1] for grep$_%2==0,0..$#program;
-test();
+compile(@program);
 
 sub similarity_for_A {
     my $A = shift;
-    my @out = @{ ( run_program( { %r, A=>$A },@program) )[0] }; #override regA
+    my @out = run_program( $A, $r{B}, $r{C} ); #override regA
     croak "A: $A   wrong out len: ".@out.", should be ".@program if @out != @program;
     my $sim = 0;
     $sim++ while $program[-$sim-1] == $out[-$sim-1] and $sim<@program;
@@ -74,10 +55,11 @@ while( @work ){
     my($wsim,$from,$to) = @{shift@work};
     $from = $form_init if $from < $from_init;
     next if $work_seen{$from,$to}++; #needed?
+    #next if $best_sim==@program and $from > $best_A; #hm
     my $diff = $to - $from;
     say "work #".++$no."   work queue: ".@work."   wsim: $wsim   from..to: $from..$to   ".
         "diff: $diff   diff len: ".length($diff)."   best similarity A: $best_A (sim $best_sim)";
-    last if $best_sim == @program; #answer found! output with register A = $A magically gives output equal to @program
+    last if $best_sim == @program; #an answer found! smallest? output with register A = $A magically gives output equal to @program
     my $step = ($to-$from) / $SAMPLES;
     my @A = $step >= 2 ? (map int($from+$_*$step), 0 .. $SAMPLES) : ($from..$to);
     for my $A (@A){
@@ -96,44 +78,49 @@ while( @work ){
 }
 my $CORRECT = $ENV{CORRECT} // 190384615275535;
 use Acme::Tools qw(avg max min srlz);
-say "Answer: $best_A ".($best_A eq $CORRECT ? 'ok   ' : 'wrong')."  samples: $SAMPLES   runtime: ".(time-$^T)."s   avg clock: ".avg(@clocks)." clocks: ".@clocks." min cl: ".min(@clocks);
-my %clock; $clock{$_}++ for @clocks;
-print srlz(\%clock,'clock','',1);
-#say "clock $_:   ".(grep{$_==)." times" for sort{$a<=>$b}uniq(@clocks);
+say "Answer: $best_A ".($best_A eq $CORRECT ? 'ok   ' : 'wrong')."  samples: $SAMPLES   runtime: ".(time-$^T)."s";
 
 __END__
 
-time SAMPLES=1000 perl 2024_day_17_part_2.pl 2024_day_17_input.txt   # 0.65 sec
+time SAMPLES=1000 perl 2024_day_17_part_2.pl 2024_day_17_input.txt   # 0.09 sec
 Answer: 190384615275535   <-------- right answer with samples 1000
 
-time SAMPLES=2000 perl 2024_day_17_part_2.pl 2024_day_17_input.txt   # ~5 sec with
+time SAMPLES=2000 perl 2024_day_17_part_2.pl 2024_day_17_input.txt   # 39 sec with sample count 2000
 Answer: 207976801319951   <-------- wrong answer with samples 2000
-
-So this program is not completely ok!
+So this program is not perfect!
 
 Imperfect idea for improvement/completion: try lots of samples counts, timeout each
 trial at 1-2 sec, and return lowest of answers found as My Answer. Probably something
-much smarter is needed for perfection.
+much smarter is needed for perfection.  Not lowest optimas are output prematurly
+when still lower candidate stretches in @work (probably) should be tried.
 
 Run:
 
-time for s in 20 40 80 160 200 220 240 300 500 1000 1500 2000; do
+time for s in 20 40 80 160 200 220 240 300 500 1000 1500 2000 3000 10000 20000 100000; do
   SAMPLES=$s CORRECT=190384615275535 perl 2024_day_17_part_2.pl 2024_day_17_input.txt
 done | grep Answer
 
+time for s in {20..1000}; do SAMPLES=$s CORRECT=190384615275535 perl 2024_day_17_part_2.pl 2024_day_17_input.txt; done | grep Answer #37% wrong
+
 Answer: 190384615275535 ok     samples: 20   runtime: 0s
 Answer: 207976801319951 wrong  samples: 40   runtime: 0s
-Answer: 190384615275535 ok     samples: 80   runtime: 1s
+Answer: 190384615275535 ok     samples: 80   runtime: 0s
 Answer: 190384625499151 wrong  samples: 160   runtime: 0s
 Answer: 190384625499151 wrong  samples: 200   runtime: 0s
-Answer: 190384615275535 ok     samples: 220   runtime: 4s
-Answer: 190384615275535 ok     samples: 240   runtime: 1s
+Answer: 190384615275535 ok     samples: 220   runtime: 1s
+Answer: 190384615275535 ok     samples: 240   runtime: 0s
 Answer: 190384625499151 wrong  samples: 300   runtime: 0s
 Answer: 190384615275535 ok     samples: 500   runtime: 0s
-Answer: 190384615275535 ok     samples: 1000   runtime: 1s
-Answer: 190384615275535 ok     samples: 1500   runtime: 1s
-Answer: 207976801319951 wrong  samples: 2000   runtime: 388s
+Answer: 190384615275535 ok     samples: 1000   runtime: 0s
+Answer: 190384615275535 ok     samples: 1500   runtime: 0s
+Answer: 207976801319951 wrong  samples: 2000   runtime: 39s
+Answer: 207976801319951 wrong  samples: 3000   runtime: 91s
+Answer: 190384615275535 ok     samples: 10000   runtime: 0s
+Answer: 190384625499151 wrong  samples: 20000   runtime: 0s
+Answer: 190384625499151 wrong  samples: 100000   runtime: 3s
 
+
+----the example----
 
 time for s in 20 40 80 160 200 220 240 300 500 1000 1500 2000; do
   SAMPLES=$s CORRECT=117440 perl 2024_day_17_part_2.pl 2024_day_17_example2.txt
