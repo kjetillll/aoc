cdd63a39a0b7a30eee5d9db1f5b3cc1e5237d8df
KjetilS <kjetilskotheim@gmail.com>
2024-12-22 20:39:39 +0100

    hm

--- advent_of_code_2024/2024_day_22_part_2.pl
+++ advent_of_code_2024/2024_day_22_part_2.pl
@@ -3,17 +3,17 @@ my %seqsum;
 while(<>){
     my($s, $prev, @diff, %seen) = ($_);
     for( 1 .. 2000 ){
-	my $ones = $s % 10;
-	push @diff, $ones - $prev if defined $prev;
-	if( @diff == 4 ){
-	    my $seq = "@diff";
-	    $seqsum{$seq} += $ones if !$seen{$seq}++;
-	    shift @diff;
-	}
-	$prev = $ones;
-	($s ^= $s <<  6) %= 16777216; # * 64 % 1<<24
-	($s ^= $s >>  5) %= 16777216; # / 32
-	($s ^= $s << 11) %= 16777216; # * 2048
+        my $ones = $s % 10;
+        push @diff, $ones - $prev if defined $prev;
+        if( @diff == 4 ){
+            my $seq = "@diff";
+            $seqsum{$seq} += $ones if !$seen{$seq}++;
+            shift @diff;
+        }
+        $prev = $ones;
+        ($s ^= $s <<  6) %= 16777216; # * 64 % 1<<24
+        ($s ^= $s >>  5) %= 16777216; # / 32
+        ($s ^= $s << 11) %= 16777216; # * 2048
     }
 }
 say "seqs: " . keys %seqsum;
